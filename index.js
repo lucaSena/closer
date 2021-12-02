2950ceb28e515c7195b65f060a0980af43fb103c
uventura <ualivent@gmail.com>
2021-12-02 11:10:26 -0300

    Photo Router

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PhotoModel = require('./models/Photo')
 // Routes
 const LoginRouter = require('./routes/login')
 const UserRouter = require('./routes/user')
+const PhotoRouter = require('./routes/photo')
 
 // Config
 app.set('view engine','ejs');
@@ -22,63 +23,11 @@ app.use(express.json());
 // Using Routes
 app.use('/', LoginRouter)
 app.use('/', UserRouter)
+app.use('/', PhotoRouter)
 
 // Main Route
 app.get('/', (req, res)=>{
     res.render('login', {title: 'Main Title'});
-})
-
-app.get('/photos',(req, res)=>{
-    res.render('test')
-});
-app.post('/image',(req, res)=>{
-   // console.log(req.body)
-    const description = req.body.description
-    const visibility = req.body.visibility == "1"
-    const show_likes = req.body.show_likes == "1"
-    const url_image = req.body.url_image
-    const user_id = 23
-    const post_date = "1999-02-02"
-    PhotoModel.create({
-        description: description,
-        visibility: visibility,
-        show_likes: show_likes,
-        image_url: url_image,
-        author_id: user_id,
-        post_date: post_date
-    }).then(()=>{
-        console.log('Inserido no banco de dados')
-        res.redirect('/')
-    }).catch((error)=>{
-        if(error)
-            throw error
-        
-        console.log('DEU MERDA')
-        res.redirect('/')
-    })
-})
-
-app.get('/image/:id', (req, res)=>{
-    const id = req.params.id
-
-    PhotoModel.findOne({
-        where:{
-            id:id
-        }
-    }).then((image)=>{
-        if(image != null)
-        {
-            res.render('image', {img_url: image['image_url']})
-        }
-        else
-        {
-            res.send('<h1>Image not founded</h1>')
-        }
-    }).catch((error)=>{
-        if(error)
-            throw error
-        console.log('Image Id Has Errors')
-    })
 })
 
 // Run Server
